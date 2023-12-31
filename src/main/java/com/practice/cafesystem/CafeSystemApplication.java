d95960bbf6991aadfc3be2330e97b4d17f2889f1
RENATU BONFIM <renatu_mb@yahoo.com.br>
2023-12-30 18:56:06 -0800

    Initial Commit

---
+++ src/main/java/com/practice/cafesystem/CafeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.practice.cafesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/cafesystem/CafeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.cafesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
